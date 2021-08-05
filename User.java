64e7bbf2006721a029307705e74ee9224a06df51
PankajSre <pankaj.saini85@gmail.com>
2021-08-05 15:26:25 +0530

    prime method added

--- User.java
+++ User.java
@@ -1,5 +1,16 @@
 public class User {
    public static void main(String[] args){
      System.out.println("Hello from User");
+	 System.out.println("Is Number Prime ? :"+ isPrime(11));
+   }
+   
+   public static int isPrime(int num){
+	   for(int i=2;i<num/2;i++){
+		   
+		   if(num%i==0)
+			   return false;
+		       break;
+	   }
+	   return true;
    }
 }
