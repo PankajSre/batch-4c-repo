98be881925d2c2834f445a58401bf7ceb86b6e46
PankajSre <pankaj.saini85@gmail.com>
2021-08-05 17:47:02 +0530

    added display method

--- Employee.java
+++ Employee.java
@@ -2,5 +2,10 @@ public class Employee {
    public static void main(String[] args){
      System.out.println("Hello Git repo");
      System.out.println("Hello Git repo");	 
+	 display();
+   }
+   
+   public static void display(){
+	   System.out.println("Hello from Display");
    }
 }
